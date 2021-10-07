b05ab9758a293cc2a773bf1aee4a90480c5f0bec
Julian Dörling <julian.doerling@ms.bulme.at>
2021-10-07 22:35:11 +0200

    Final commit

--- windmill_problem/main.cpp
+++ windmill_problem/main.cpp
@@ -6,16 +6,18 @@
 #include <math.h>
 #include <iostream>
 
+#define STEP 0.0001
+#define TOLERANCE 10000
+#define RADIUS 4.f
 #define white sf::Color::White
 
-int windowHeight = 1000;
-int windowWidth = 1000;
-float currentAngleP = 0.f;
-float currentAngleN = 180.f;
+short windowHeight = 1000;
+short windowWidth = 1000;
+double currentAngleP = 0.f;
 sf::CircleShape createCircle();
-sf::Vertex createLineStart(float xCircle, float yCircle, float currentAngle);
-sf::Vertex createLineEnd(float xCircle, float yCircle, float currentAngle);
-bool checkCollision(float xCircle, float yCircle, sf::Vertex lineStart, sf::Vertex lineEnd);
+sf::Vertex createLineStart(double xCircle, double yCircle, double currentAngle);
+sf::Vertex createLineEnd(double xCircle, double yCircle, double currentAngle);
+bool checkCollision(double xCircle, double yCircle, sf::CircleShape currentCircle, sf::Vertex lineEnd, double currentAngle, sf::CircleShape previousCircle);
 
 int main() 
 {
@@ -27,80 +29,84 @@ int main()
 
 	srand(time(0));
 
-	for (int i = 0; i < 20; i++) {
+	for (int i = 0; i < 10; i++) {
 		v.push_back(createCircle());
 	}
 
 	int vIndex = rand() % v.size();
 
 	sf::CircleShape currentRotationCenter = v[vIndex];
-	v[vIndex].setFillColor(sf::Color::Red);
-
-	window.setFramerateLimit(100);
+	sf::CircleShape previousCircle;
 
 	while (window.isOpen())
 	{
+		window.clear();
 		while (window.pollEvent(event)) 
 		{		
 			if (event.type == sf::Event::Closed)
 				window.close();
 		}
-		
 		if (currentAngleP == 360) currentAngleP = 0;
-		if (currentAngleN == 360) currentAngleN = 0;
 
-		window.clear();
 		sf::Vertex line[] =
 		{
 			createLineStart(currentRotationCenter.getPosition().x, currentRotationCenter.getPosition().y, currentAngleP),
 			createLineEnd(currentRotationCenter.getPosition().x, currentRotationCenter.getPosition().y, currentAngleP)
 		};
-		for (auto circle : v) {
+		int index = 0;
+		for (auto circle : v) 
+		{
+			if(vIndex != index)
+			v[index].setFillColor(white);
+			if (checkCollision(circle.getPosition().x, circle.getPosition().y, circle, line[1], currentAngleP, previousCircle) == true)
+			{
+				previousCircle = currentRotationCenter;
+				currentRotationCenter = circle;
+				v[vIndex].setFillColor(sf::Color::Red);	
+			}
 			window.draw(circle);
-			if (checkCollision(circle.getPosition().x, circle.getPosition().y, line[0], line[1]) == true)
-			std::cout << "True\n";
-		}		
+			index++;
+		}
 		window.draw(line, 2, sf::Lines);
 		window.display();
-		currentAngleP += 0.001;
-		currentAngleN += 0.001;
-		
+		currentAngleP += STEP;	
 	}
 	return 0;
 }
 
 sf::CircleShape createCircle()
 {
-	sf::CircleShape circle(4.f);
+	sf::CircleShape circle(RADIUS);
 	circle.setFillColor(white);
-	float x = windowWidth / 20 + rand() % (windowWidth - windowWidth/10);
-	float y = windowHeight / 20 + rand() % (windowHeight - windowHeight / 10);
+	double x = round(windowWidth / 10 + rand() % (windowWidth - windowWidth / 5));
+	double y = round(windowHeight / 10 + rand() % (windowHeight - windowHeight / 5));
 	circle.move(x, y);
 	return circle;
 }
 
-sf::Vertex createLineStart(float xCircle, float yCircle, float currentAngle) 
+sf::Vertex createLineStart(double xCircle, double yCircle, double currentAngle) 
 {
-	float x = xCircle;
-	float y = yCircle;
-	return sf::Vertex(sf::Vector2f((x + windowWidth * 1.41 * cos(currentAngle)) + 4.f, (y + windowWidth * 1.41 * sin(currentAngle)) + 4.f));
+	double x = xCircle;
+	double y = yCircle;
+	return sf::Vertex(sf::Vector2f((x + windowWidth * 1.41 * cos(currentAngle)) + RADIUS, (y + windowWidth * 1.41 * sin(currentAngle)) + RADIUS));
 }
 
-sf::Vertex createLineEnd(float xCircle, float yCircle, float currentAngle)
+sf::Vertex createLineEnd(double xCircle, double yCircle, double currentAngle)
 {	
-	float x = xCircle;
-	float y = yCircle;
-	return sf::Vertex(sf::Vector2f((x + (-1.f) * (windowWidth * 1.41 * cos(currentAngle))) + 4.f, (y + (-1.f) * (windowWidth * 1.41 * sin(currentAngle))) + 4.f));
+	double x = xCircle;
+	double y = yCircle;
+	return sf::Vertex(sf::Vector2f((x + (-1.f) * (windowWidth * 1.41 * cos(currentAngle))) + RADIUS, (y + (-1.f) * (windowWidth * 1.41 * sin(currentAngle))) + RADIUS));
 }
 
-bool checkCollision(float xCircle, float yCircle, sf::Vertex lineStart, sf::Vertex lineEnd)
+bool checkCollision(double xCircle, double yCircle, sf::CircleShape currentCircle, sf::Vertex lineEnd, double currentAngle, sf::CircleShape previousCircle)
 {
-	float checkA = round(((xCircle + 4.f) / lineEnd.position.x) - lineStart.position.x);
-	float checkB = round(((yCircle + 4.f) / lineEnd.position.y) - lineStart.position.y);
-	std::cout << checkA << " == ";
-	std::cout << checkB << std::endl;
-	if (checkA != checkB) return false;
-	return true;
+	if (currentCircle.getPosition().x != previousCircle.getPosition().x && currentCircle.getPosition().y != previousCircle.getPosition().y) {
+		double k = tan(currentAngle);
+		double d = lineEnd.position.y - k * lineEnd.position.x;
+		double y = round(k * (xCircle + RADIUS) + d);
+		return (y >= yCircle - (STEP * TOLERANCE) && y <= yCircle + (STEP * TOLERANCE)) ? true : false;
+	}
+	return false;
 }
 
 
